eec530517583faf534458dc9540b9f328eaaa3db
1337NamNori <nam20051999@gmail.com>
2021-09-14 00:05:45 +0700

    Actor component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,6 +9,7 @@ import BreadCrumb from "./BreadCrumb";
 import MovieInfo from "./MovieInfo";
 import MovieInfoBar from "./MovieInfoBar";
 import Grid from "./Grid";
+import Actor from "./Actor";
 import Spinner from "./Spinner";
 
 // Hooks
@@ -32,6 +33,20 @@ const Movie = () => {
             <BreadCrumb movieTitle={movie.original_title} />
             <MovieInfo movie={movie} />
             <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
+            <Grid header="Actors">
+                {movie.actors.map(actor => (
+                   <Actor
+                       key={actor.credit_id}
+                       name={actor.name}
+                       character={actor.character}
+                       imageUrl={
+                           actor.profile_path
+                               ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                               : NoImage
+                       }
+                   />
+                ))}
+            </Grid>
         </>
     )
 }
