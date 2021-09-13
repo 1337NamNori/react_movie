167e82da7cb0b3a1f67cfe989ec7652e07045b7d
1337NamNori <nam20051999@gmail.com>
2021-09-13 18:50:03 +0700

    SearchBar component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from "../config";
 
 // Components
 import HeroImage from "./HeroImage";
+import SearchBar from "./SearchBar";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
@@ -16,20 +17,21 @@ import {useHomeFetch} from "../hooks/useHomeFetch";
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-    const {state, error, loading} = useHomeFetch();
+    const {state, error, loading, setSearchTerm, searchTerm} = useHomeFetch();
 
     console.log(state);
 
     return (
         <>
-            {state.results[0] &&
+            {!searchTerm && state.results[0] &&
                 <HeroImage
                     image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
                     title={state.results[0].original_title}
                     text={state.results[0].overview}
                 />
             }
-            <Grid header='Popular Movies'>
+            <SearchBar setSearchTerm={setSearchTerm}/>
+            <Grid header={searchTerm ? `Search Results for '${searchTerm}'` : 'Popular Movies'}>
                 {state.results.map(movie => (
                     <Thumb key={movie.id} clickable movieId={movie.id}
                            image={
